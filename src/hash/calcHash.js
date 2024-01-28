fa925fe8c5c6bc71dfac370db70ec4c228eadfde
Your Name <oubowen@MacBook-Air.local>
2024-01-28 22:13:22 +0300

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { resolve } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = resolve(
+    'src',
+    'hash',
+    'files',
+    'fileToCalculateHashFor.txt'
+  );
+  const hash = createHash('sha256');
+  const stream = createReadStream(filePath);
+
+  stream.on('data', (data) => {
+    hash.update(data);
+  });
+
+  stream.on('end', () => {
+    const hexHash = hash.digest('hex');
+    console.log(hexHash);
+  });
+
+  stream.on('error', (error) => {
+    console.error(error.message);
+  });
 };
 
 await calculateHash();
