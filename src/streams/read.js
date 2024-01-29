e8c799e8a67ff5bae3e9d6a13420ef552f69be35
Your Name <oubowen@MacBook-Air.local>
2024-01-29 10:30:12 +0300

    feat: complete streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs';
+import { resolve } from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = resolve('src', 'streams', 'files', 'fileToRead.txt');
+
+  const stream = createReadStream(filePath);
+
+  stream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  stream.on('error', (error) => {
+    console.error(error.message);
+  });
 };
 
 await read();
